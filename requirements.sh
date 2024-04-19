40810da77242402b0fa15acdd73ef78e5172143c
Rasse <rassemichael@gmail.com>
2024-04-20 00:01:18 +0300

    Small tweak
    Old file didn't install anything

--- requirements.sh
+++ requirements.sh
@@ -1,4 +1,4 @@
-install_packages() {
+install_package() {
     sudo apt-get update
     sudo apt-get install $1 -y
 }
@@ -9,31 +9,31 @@ install_packages() {
 sudo apt-get update
 sudo apt-get upgrade -y
 
-install_packages curl
+install_package curl
+
+# Git
+# ===
+install_package git
 
 # Brave browser
 # =============
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+install_package brave-browser
 
 # Github-desktop
 # ==============
 wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/shiftkey-packages.gpg > /dev/null
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" > /etc/apt/sources.list.d/shiftkey-packages.list'
+install_package github-desktop
 
 # Spotify
 # =======
 curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-
+install_package spotify-client
 
 # Other
 # =====
-sudo add-apt-repository universe
-
-
-# Install all packages
-install_packages git \
-    brave-browser \
-    github-desktop \
-    spotify-client \
+sudo add-apt-repository universe -y
+install_package gnome-tweaks
