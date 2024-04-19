3bce7735ac8a864fed077efb09d75bf32aed1006
Rasse <rassemichael@gmail.com>
2024-04-19 23:53:50 +0300

    First commit
    Should contain all the basic programs needed for a basic linux system

---
+++ requirements.sh
@@ -0,0 +1,39 @@
+install_packages() {
+    sudo apt-get update
+    sudo apt-get install $1 -y
+}
+
+
+# Update and upgrade
+# ==================
+sudo apt-get update
+sudo apt-get upgrade -y
+
+install_packages curl
+
+# Brave browser
+# =============
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+# Github-desktop
+# ==============
+wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/shiftkey-packages.gpg > /dev/null
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" > /etc/apt/sources.list.d/shiftkey-packages.list'
+
+# Spotify
+# =======
+curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+
+
+# Other
+# =====
+sudo add-apt-repository universe
+
+
+# Install all packages
+install_packages git \
+    brave-browser \
+    github-desktop \
+    spotify-client \
